214975583f8e3f0561781b07392fcdee956b2ec8
Saksham Gupta <saksham757gupta@gmail.com>
2024-01-16 22:42:47 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationFromComponent } from './reservation-from/reservation-from.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'new',
+    component: ReservationFromComponent
+  },
+  {
+    path: 'list',
+    component: ReservationListComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
