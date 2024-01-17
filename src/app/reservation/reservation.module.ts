1669d9ecd5601280c5e2d67625cc6f0d39231b2f
Saksham Gupta <saksham757gupta@gmail.com>
2024-01-17 20:05:37 +0530

    completed

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,6 +4,7 @@ import { ReservationFromComponent } from '../reservation-from/reservation-from.c
 import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HomeModule } from '../home/home.module';
 
 
 
@@ -16,7 +17,8 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    HomeModule
   ]
 })
 export class ReservationModule { }
